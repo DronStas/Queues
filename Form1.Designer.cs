33c80a89f0bb6ae1b6d13b7c407bc7121a6d0be7
danik <baybolov_daniyar@mail.ru>
2022-03-06 21:45:26 +0600

    add

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,19 +30,19 @@ namespace Queues
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Pause_btn = new System.Windows.Forms.Button();
             this.Start_btn = new System.Windows.Forms.Button();
+            this.Pause_btn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.Name_kas1 = new System.Windows.Forms.Label();
-            this.Name_kas2 = new System.Windows.Forms.Label();
-            this.Name_kas3 = new System.Windows.Forms.Label();
-            this.Name_klient = new System.Windows.Forms.Label();
-            this.dgv_klient = new System.Windows.Forms.DataGridView();
-            this.ListKas1 = new System.Windows.Forms.ListBox();
-            this.ListKas2 = new System.Windows.Forms.ListBox();
             this.ListKas3 = new System.Windows.Forms.ListBox();
+            this.ListKas2 = new System.Windows.Forms.ListBox();
+            this.ListKas1 = new System.Windows.Forms.ListBox();
+            this.dgv_klient = new System.Windows.Forms.DataGridView();
+            this.Name_klient = new System.Windows.Forms.Label();
+            this.Name_kas3 = new System.Windows.Forms.Label();
+            this.Name_kas2 = new System.Windows.Forms.Label();
+            this.Name_kas1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel2.SuspendLayout();
@@ -62,26 +62,18 @@ namespace Queues
             this.panel1.Size = new System.Drawing.Size(200, 578);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // Start_btn
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Lavender;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 26);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 26);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(200, 77);
-            this.dataGridView1.TabIndex = 1;
+            this.Start_btn.BackColor = System.Drawing.Color.Transparent;
+            this.Start_btn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Start_btn.FlatAppearance.BorderSize = 0;
+            this.Start_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Start_btn.Location = new System.Drawing.Point(0, 478);
+            this.Start_btn.Name = "Start_btn";
+            this.Start_btn.Size = new System.Drawing.Size(200, 50);
+            this.Start_btn.TabIndex = 3;
+            this.Start_btn.Text = "Старт";
+            this.Start_btn.UseVisualStyleBackColor = false;
             // 
             // Pause_btn
             // 
@@ -95,18 +87,26 @@ namespace Queues
             this.Pause_btn.Text = "Пауза";
             this.Pause_btn.UseVisualStyleBackColor = false;
             // 
-            // Start_btn
+            // dataGridView1
             // 
-            this.Start_btn.BackColor = System.Drawing.Color.Transparent;
-            this.Start_btn.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.Start_btn.FlatAppearance.BorderSize = 0;
-            this.Start_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Start_btn.Location = new System.Drawing.Point(0, 478);
-            this.Start_btn.Name = "Start_btn";
-            this.Start_btn.Size = new System.Drawing.Size(200, 50);
-            this.Start_btn.TabIndex = 3;
-            this.Start_btn.Text = "Старт";
-            this.Start_btn.UseVisualStyleBackColor = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(200, 77);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lavender;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
             // 
             // panel2
             // 
@@ -125,45 +125,35 @@ namespace Queues
             this.panel2.Size = new System.Drawing.Size(1061, 578);
             this.panel2.TabIndex = 1;
             // 
-            // Name_kas1
+            // ListKas3
             // 
-            this.Name_kas1.AutoSize = true;
-            this.Name_kas1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Name_kas1.Location = new System.Drawing.Point(36, 55);
-            this.Name_kas1.Name = "Name_kas1";
-            this.Name_kas1.Size = new System.Drawing.Size(105, 24);
-            this.Name_kas1.TabIndex = 0;
-            this.Name_kas1.Text = "Касса №1";
+            this.ListKas3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ListKas3.FormattingEnabled = true;
+            this.ListKas3.Location = new System.Drawing.Point(496, 81);
+            this.ListKas3.Name = "ListKas3";
+            this.ListKas3.Size = new System.Drawing.Size(151, 485);
+            this.ListKas3.TabIndex = 7;
             // 
-            // Name_kas2
+            // ListKas2
             // 
-            this.Name_kas2.AutoSize = true;
-            this.Name_kas2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Name_kas2.Location = new System.Drawing.Point(263, 55);
-            this.Name_kas2.Name = "Name_kas2";
-            this.Name_kas2.Size = new System.Drawing.Size(105, 24);
-            this.Name_kas2.TabIndex = 1;
-            this.Name_kas2.Text = "Касса №2";
+            this.ListKas2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ListKas2.FormattingEnabled = true;
+            this.ListKas2.Location = new System.Drawing.Point(266, 81);
+            this.ListKas2.Name = "ListKas2";
+            this.ListKas2.Size = new System.Drawing.Size(151, 485);
+            this.ListKas2.TabIndex = 6;
             // 
-            // Name_kas3
+            // ListKas1
             // 
-            this.Name_kas3.AutoSize = true;
-            this.Name_kas3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Name_kas3.Location = new System.Drawing.Point(493, 55);
-            this.Name_kas3.Name = "Name_kas3";
-            this.Name_kas3.Size = new System.Drawing.Size(105, 24);
-            this.Name_kas3.TabIndex = 2;
-            this.Name_kas3.Text = "Касса №3";
-            // 
-            // Name_klient
-            // 
-            this.Name_klient.AutoSize = true;
-            this.Name_klient.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Name_klient.Location = new System.Drawing.Point(700, 55);
-            this.Name_klient.Name = "Name_klient";
-            this.Name_klient.Size = new System.Drawing.Size(95, 24);
-            this.Name_klient.TabIndex = 3;
-            this.Name_klient.Text = "Клиенты";
+            this.ListKas1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ListKas1.FormattingEnabled = true;
+            this.ListKas1.Location = new System.Drawing.Point(39, 81);
+            this.ListKas1.Name = "ListKas1";
+            this.ListKas1.Size = new System.Drawing.Size(151, 485);
+            this.ListKas1.TabIndex = 5;
             // 
             // dgv_klient
             // 
@@ -176,35 +166,45 @@ namespace Queues
             this.dgv_klient.Size = new System.Drawing.Size(346, 483);
             this.dgv_klient.TabIndex = 4;
             // 
-            // ListKas1
+            // Name_klient
             // 
-            this.ListKas1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.ListKas1.FormattingEnabled = true;
-            this.ListKas1.Location = new System.Drawing.Point(39, 81);
-            this.ListKas1.Name = "ListKas1";
-            this.ListKas1.Size = new System.Drawing.Size(151, 485);
-            this.ListKas1.TabIndex = 5;
+            this.Name_klient.AutoSize = true;
+            this.Name_klient.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name_klient.Location = new System.Drawing.Point(700, 55);
+            this.Name_klient.Name = "Name_klient";
+            this.Name_klient.Size = new System.Drawing.Size(95, 24);
+            this.Name_klient.TabIndex = 3;
+            this.Name_klient.Text = "Клиенты";
             // 
-            // ListKas2
+            // Name_kas3
             // 
-            this.ListKas2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.ListKas2.FormattingEnabled = true;
-            this.ListKas2.Location = new System.Drawing.Point(266, 81);
-            this.ListKas2.Name = "ListKas2";
-            this.ListKas2.Size = new System.Drawing.Size(151, 485);
-            this.ListKas2.TabIndex = 6;
+            this.Name_kas3.AutoSize = true;
+            this.Name_kas3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name_kas3.Location = new System.Drawing.Point(493, 55);
+            this.Name_kas3.Name = "Name_kas3";
+            this.Name_kas3.Size = new System.Drawing.Size(105, 24);
+            this.Name_kas3.TabIndex = 2;
+            this.Name_kas3.Text = "Касса №3";
             // 
-            // ListKas3
+            // Name_kas2
             // 
-            this.ListKas3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.ListKas3.FormattingEnabled = true;
-            this.ListKas3.Location = new System.Drawing.Point(496, 81);
-            this.ListKas3.Name = "ListKas3";
-            this.ListKas3.Size = new System.Drawing.Size(151, 485);
-            this.ListKas3.TabIndex = 7;
+            this.Name_kas2.AutoSize = true;
+            this.Name_kas2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name_kas2.Location = new System.Drawing.Point(263, 55);
+            this.Name_kas2.Name = "Name_kas2";
+            this.Name_kas2.Size = new System.Drawing.Size(105, 24);
+            this.Name_kas2.TabIndex = 1;
+            this.Name_kas2.Text = "Касса №2";
+            // 
+            // Name_kas1
+            // 
+            this.Name_kas1.AutoSize = true;
+            this.Name_kas1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name_kas1.Location = new System.Drawing.Point(36, 55);
+            this.Name_kas1.Name = "Name_kas1";
+            this.Name_kas1.Size = new System.Drawing.Size(105, 24);
+            this.Name_kas1.TabIndex = 0;
+            this.Name_kas1.Text = "Касса №1";
             // 
             // Form1
             // 
