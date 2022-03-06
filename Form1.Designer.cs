2ddd85333ecd439300c52a9f9fd431118d6f1afc
danik <baybolov_daniyar@mail.ru>
2022-03-06 23:01:06 +0600

    nice

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,14 +37,14 @@ namespace Queues
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.ListKas3 = new System.Windows.Forms.ListBox();
+            this.ListKas2 = new System.Windows.Forms.ListBox();
             this.ListKas1 = new System.Windows.Forms.ListBox();
             this.dgv_klient = new System.Windows.Forms.DataGridView();
             this.Name_klient = new System.Windows.Forms.Label();
             this.Name_kas3 = new System.Windows.Forms.Label();
             this.Name_kas2 = new System.Windows.Forms.Label();
             this.Name_kas1 = new System.Windows.Forms.Label();
-            this.ListKas2 = new System.Windows.Forms.ListBox();
-            this.ListKas3 = new System.Windows.Forms.ListBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel2.SuspendLayout();
@@ -61,9 +61,8 @@ namespace Queues
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(267, 711);
+            this.panel1.Size = new System.Drawing.Size(200, 578);
             this.panel1.TabIndex = 0;
             // 
             // Start_btn
@@ -77,10 +76,9 @@ namespace Queues
             this.Start_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Start_btn.Font = new System.Drawing.Font("Forte", 25.8F);
             this.Start_btn.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.Start_btn.Location = new System.Drawing.Point(0, 587);
-            this.Start_btn.Margin = new System.Windows.Forms.Padding(4);
+            this.Start_btn.Location = new System.Drawing.Point(0, 478);
             this.Start_btn.Name = "Start_btn";
-            this.Start_btn.Size = new System.Drawing.Size(267, 62);
+            this.Start_btn.Size = new System.Drawing.Size(200, 50);
             this.Start_btn.TabIndex = 3;
             this.Start_btn.Text = "Start";
             this.Start_btn.UseVisualStyleBackColor = false;
@@ -92,10 +90,9 @@ namespace Queues
             this.Pause_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Pause_btn.Font = new System.Drawing.Font("Forte", 25.8F);
             this.Pause_btn.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.Pause_btn.Location = new System.Drawing.Point(0, 649);
-            this.Pause_btn.Margin = new System.Windows.Forms.Padding(4);
+            this.Pause_btn.Location = new System.Drawing.Point(0, 528);
             this.Pause_btn.Name = "Pause_btn";
-            this.Pause_btn.Size = new System.Drawing.Size(267, 62);
+            this.Pause_btn.Size = new System.Drawing.Size(200, 50);
             this.Pause_btn.TabIndex = 2;
             this.Pause_btn.Text = "Pause";
             this.Pause_btn.UseVisualStyleBackColor = false;
@@ -113,13 +110,12 @@ namespace Queues
             this.Column1,
             this.Column2});
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 47);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 38);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(267, 95);
+            this.dataGridView1.Size = new System.Drawing.Size(200, 77);
             this.dataGridView1.TabIndex = 1;
             // 
             // Column1
@@ -142,9 +138,8 @@ namespace Queues
             this.label1.Font = new System.Drawing.Font("Forte", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 47);
+            this.label1.Size = new System.Drawing.Size(94, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "Time:";
             // 
@@ -160,116 +155,11 @@ namespace Queues
             this.panel2.Controls.Add(this.Name_kas2);
             this.panel2.Controls.Add(this.Name_kas1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(267, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Location = new System.Drawing.Point(200, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1414, 711);
+            this.panel2.Size = new System.Drawing.Size(1061, 578);
             this.panel2.TabIndex = 1;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
-            // 
-            // ListKas1
-            // 
-            this.ListKas1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.ListKas1.BackColor = System.Drawing.Color.SkyBlue;
-            this.ListKas1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.ListKas1.Font = new System.Drawing.Font("Arial Narrow", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ListKas1.ForeColor = System.Drawing.Color.DarkOrange;
-            this.ListKas1.FormattingEnabled = true;
-            this.ListKas1.ItemHeight = 69;
-            this.ListKas1.Items.AddRange(new object[] {
-            "1",
-            "2",
-            "3",
-            "4",
-            "5"});
-            this.ListKas1.Location = new System.Drawing.Point(52, 100);
-            this.ListKas1.Margin = new System.Windows.Forms.Padding(4);
-            this.ListKas1.Name = "ListKas1";
-            this.ListKas1.Size = new System.Drawing.Size(200, 554);
-            this.ListKas1.TabIndex = 5;
-            this.ListKas1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.ListKas1_DrawItem);
-            // 
-            // dgv_klient
-            // 
-            this.dgv_klient.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dgv_klient.BackgroundColor = System.Drawing.Color.SkyBlue;
-            this.dgv_klient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_klient.Location = new System.Drawing.Point(937, 100);
-            this.dgv_klient.Margin = new System.Windows.Forms.Padding(4);
-            this.dgv_klient.Name = "dgv_klient";
-            this.dgv_klient.RowHeadersVisible = false;
-            this.dgv_klient.RowHeadersWidth = 51;
-            this.dgv_klient.Size = new System.Drawing.Size(460, 594);
-            this.dgv_klient.TabIndex = 4;
-            // 
-            // Name_klient
-            // 
-            this.Name_klient.AutoSize = true;
-            this.Name_klient.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold);
-            this.Name_klient.Location = new System.Drawing.Point(931, 34);
-            this.Name_klient.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.Name_klient.Name = "Name_klient";
-            this.Name_klient.Size = new System.Drawing.Size(124, 35);
-            this.Name_klient.TabIndex = 3;
-            this.Name_klient.Text = "Клиенты";
-            // 
-            // Name_kas3
-            // 
-            this.Name_kas3.AutoSize = true;
-            this.Name_kas3.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold);
-            this.Name_kas3.Location = new System.Drawing.Point(655, 34);
-            this.Name_kas3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.Name_kas3.Name = "Name_kas3";
-            this.Name_kas3.Size = new System.Drawing.Size(134, 35);
-            this.Name_kas3.TabIndex = 2;
-            this.Name_kas3.Text = "Касса №3";
-            // 
-            // Name_kas2
-            // 
-            this.Name_kas2.AutoSize = true;
-            this.Name_kas2.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold);
-            this.Name_kas2.Location = new System.Drawing.Point(350, 34);
-            this.Name_kas2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.Name_kas2.Name = "Name_kas2";
-            this.Name_kas2.Size = new System.Drawing.Size(134, 35);
-            this.Name_kas2.TabIndex = 1;
-            this.Name_kas2.Text = "Касса №2";
-            // 
-            // Name_kas1
-            // 
-            this.Name_kas1.AutoSize = true;
-            this.Name_kas1.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Name_kas1.Location = new System.Drawing.Point(46, 28);
-            this.Name_kas1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.Name_kas1.Name = "Name_kas1";
-            this.Name_kas1.Size = new System.Drawing.Size(134, 35);
-            this.Name_kas1.TabIndex = 0;
-            this.Name_kas1.Text = "Касса №1";
-            // 
-            // ListKas2
-            // 
-            this.ListKas2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.ListKas2.BackColor = System.Drawing.Color.SkyBlue;
-            this.ListKas2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.ListKas2.Font = new System.Drawing.Font("Arial Narrow", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ListKas2.ForeColor = System.Drawing.Color.DarkOrange;
-            this.ListKas2.FormattingEnabled = true;
-            this.ListKas2.ItemHeight = 69;
-            this.ListKas2.Items.AddRange(new object[] {
-            "1",
-            "2",
-            "3",
-            "4",
-            "5"});
-            this.ListKas2.Location = new System.Drawing.Point(356, 100);
-            this.ListKas2.Margin = new System.Windows.Forms.Padding(4);
-            this.ListKas2.Name = "ListKas2";
-            this.ListKas2.Size = new System.Drawing.Size(200, 485);
-            this.ListKas2.TabIndex = 6;
             // 
             // ListKas3
             // 
@@ -280,27 +170,122 @@ namespace Queues
             this.ListKas3.Font = new System.Drawing.Font("Arial Narrow", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ListKas3.ForeColor = System.Drawing.Color.DarkOrange;
             this.ListKas3.FormattingEnabled = true;
-            this.ListKas3.ItemHeight = 69;
+            this.ListKas3.ItemHeight = 57;
             this.ListKas3.Items.AddRange(new object[] {
             "1",
             "2",
             "3",
             "4",
             "5"});
-            this.ListKas3.Location = new System.Drawing.Point(661, 100);
-            this.ListKas3.Margin = new System.Windows.Forms.Padding(4);
+            this.ListKas3.Location = new System.Drawing.Point(496, 81);
             this.ListKas3.Name = "ListKas3";
-            this.ListKas3.Size = new System.Drawing.Size(200, 485);
+            this.ListKas3.Size = new System.Drawing.Size(150, 458);
             this.ListKas3.TabIndex = 7;
+            // 
+            // ListKas2
+            // 
+            this.ListKas2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ListKas2.BackColor = System.Drawing.Color.SkyBlue;
+            this.ListKas2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ListKas2.Font = new System.Drawing.Font("Arial Narrow", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ListKas2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.ListKas2.FormattingEnabled = true;
+            this.ListKas2.ItemHeight = 57;
+            this.ListKas2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.ListKas2.Location = new System.Drawing.Point(267, 81);
+            this.ListKas2.Name = "ListKas2";
+            this.ListKas2.Size = new System.Drawing.Size(150, 458);
+            this.ListKas2.TabIndex = 6;
+            // 
+            // ListKas1
+            // 
+            this.ListKas1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ListKas1.BackColor = System.Drawing.Color.SkyBlue;
+            this.ListKas1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ListKas1.Font = new System.Drawing.Font("Arial Narrow", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ListKas1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.ListKas1.FormattingEnabled = true;
+            this.ListKas1.ItemHeight = 57;
+            this.ListKas1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.ListKas1.Location = new System.Drawing.Point(39, 81);
+            this.ListKas1.Name = "ListKas1";
+            this.ListKas1.Size = new System.Drawing.Size(150, 458);
+            this.ListKas1.TabIndex = 5;
+            this.ListKas1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.ListKas1_DrawItem);
+            // 
+            // dgv_klient
+            // 
+            this.dgv_klient.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv_klient.BackgroundColor = System.Drawing.Color.SkyBlue;
+            this.dgv_klient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_klient.Location = new System.Drawing.Point(703, 81);
+            this.dgv_klient.Name = "dgv_klient";
+            this.dgv_klient.RowHeadersVisible = false;
+            this.dgv_klient.RowHeadersWidth = 51;
+            this.dgv_klient.Size = new System.Drawing.Size(346, 483);
+            this.dgv_klient.TabIndex = 4;
+            // 
+            // Name_klient
+            // 
+            this.Name_klient.AutoSize = true;
+            this.Name_klient.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold);
+            this.Name_klient.Location = new System.Drawing.Point(698, 28);
+            this.Name_klient.Name = "Name_klient";
+            this.Name_klient.Size = new System.Drawing.Size(101, 29);
+            this.Name_klient.TabIndex = 3;
+            this.Name_klient.Text = "Клиенты";
+            // 
+            // Name_kas3
+            // 
+            this.Name_kas3.AutoSize = true;
+            this.Name_kas3.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold);
+            this.Name_kas3.Location = new System.Drawing.Point(491, 28);
+            this.Name_kas3.Name = "Name_kas3";
+            this.Name_kas3.Size = new System.Drawing.Size(107, 29);
+            this.Name_kas3.TabIndex = 2;
+            this.Name_kas3.Text = "Касса №3";
+            // 
+            // Name_kas2
+            // 
+            this.Name_kas2.AutoSize = true;
+            this.Name_kas2.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold);
+            this.Name_kas2.Location = new System.Drawing.Point(262, 28);
+            this.Name_kas2.Name = "Name_kas2";
+            this.Name_kas2.Size = new System.Drawing.Size(107, 29);
+            this.Name_kas2.TabIndex = 1;
+            this.Name_kas2.Text = "Касса №2";
+            // 
+            // Name_kas1
+            // 
+            this.Name_kas1.AutoSize = true;
+            this.Name_kas1.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name_kas1.Location = new System.Drawing.Point(34, 23);
+            this.Name_kas1.Name = "Name_kas1";
+            this.Name_kas1.Size = new System.Drawing.Size(107, 29);
+            this.Name_kas1.TabIndex = 0;
+            this.Name_kas1.Text = "Касса №1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1681, 711);
+            this.ClientSize = new System.Drawing.Size(1261, 578);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
